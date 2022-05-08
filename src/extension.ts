3b687f45bab051ba835e2b75beaab41af9ab2187
刘伟 <liuwei.felix@bytedance.com>
2022-05-08 17:33:05 +0800

    chore: 删除无用代码

--- src/extension.ts
+++ src/extension.ts
@@ -4,27 +4,6 @@ import { uploadImageCommand } from '././commands/upload-image';
 import { markdownImagePreviewCommand } from './commands/image-preview';
 import { markdownSettingCommand } from './commands/markdown-settings';
 
-function getWebviewContent() {
-  return `
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <title>Cat Coding</title>
-          </head>
-          <body>
-              <div id="root">0</div>
-              <div id=btn>开始</div>
-
-              <script>
-                  (function() {}())
-              </script>
-          </body>
-          </html>
-      `;
-}
-
 export function activate(context: vscode.ExtensionContext) {
   const uploadImage = vscode.commands.registerCommand(
     ...markdownSettingCommand(context)
@@ -37,38 +16,10 @@ export function activate(context: vscode.ExtensionContext) {
     ...markdownImagePreviewCommand(context)
   );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('test', () => {
-      const panel = vscode.window.createWebviewPanel(
-        'catCoding',
-        'Cat Coding',
-        vscode.ViewColumn.One,
-        {
-          enableScripts: true,
-        }
-      );
-
-      panel.webview.html = getWebviewContent();
-
-      // 处理webview中的信息
-      panel.webview.onDidReceiveMessage(
-        (message) => {
-          switch (message.command) {
-            case 'alert':
-              vscode.window.showErrorMessage(message.text);
-              return;
-          }
-        },
-        undefined,
-        context.subscriptions
-      );
-    })
-  );
   context.subscriptions.push(uploadImage);
   context.subscriptions.push(markdownSettings);
   context.subscriptions.push(markdownImagePreview);
-  // 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
-  // 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
+ 
 }
 
 export function deactivate() {}
